7943007e3065ccc5984c26f0e1e5e3daf56d1414
Erick Zanardo <erickzanardoo@gmail.com>
2019-07-06 15:34:29 -0300

    Adding coins

--- game/lib/screens/game/controller.dart
+++ game/lib/screens/game/controller.dart
@@ -5,8 +5,9 @@ import 'package:flame/time.dart';
 import 'dart:ui';
 import 'dart:math';
 
-import './enemy.dart';
-import './game.dart';
+import 'enemy.dart';
+import 'coin.dart';
+import 'game.dart';
 
 class GameController extends PositionComponent {
   static final Paint white = Paint()..color = Color(0xFFFFFFFF);
@@ -18,12 +19,16 @@ class GameController extends PositionComponent {
   final Game gameRef;
 
   Rect _background;
-  Position _scorePosition = Position(10, 50);
+  Position _scorePosition = Position(10, 10);
+  Position _coinsPosition = Position(10, 30);
 
   Timer enemyCreator;
   Timer enemySpeedIncreaser;
 
+  Timer coinCreator;
+
   int _score = 0;
+  int _coins = 0;
 
   GameController(this.gameRef) {
     _background = Rect.fromLTWH(0, 0, gameRef.size.width, gameRef.size.height);
@@ -39,25 +44,42 @@ class GameController extends PositionComponent {
       gameRef.currentEnemySpeed = min(gameRef.currentEnemySpeed + Game.ENEMY_SPEED_STEP, Game.MAX_ENEMY_SPEED);
     });
     enemySpeedIncreaser.start();
+
+    coinCreator = Timer(4, repeat: true, callback: () {
+      if (random.nextDouble() >= 0.6) {
+        gameRef.add(CoinComponent(
+          gameRef,
+          random.nextInt((gameRef.size.width - 8).toInt()).toDouble(),
+        ));
+      }
+    });
+    coinCreator.start();
   }
 
   @override
   void update(double dt) {
     enemyCreator.update(dt);
     enemySpeedIncreaser.update(dt);
+    coinCreator.update(dt);
   }
 
   void resetScore() {
     gameRef.currentEnemySpeed = Game.INITIAL_ENEMY_SPEED;
     _score = 0;
+    _coins = 0;
   }
 
   void increaseScore() {
     _score++;
   }
 
+  void increaseCoins() {
+    _coins++;
+  }
+
   @override
   void render(Canvas canvas) {
     scoreTextConfig.render(canvas, "Score: $_score", _scorePosition);
+    scoreTextConfig.render(canvas, "Coins: $_coins", _coinsPosition);
   }
 }
