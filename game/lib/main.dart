03fc765225ccae4afc7aaf0c76ac7e280bbe50ce
Erick Zanardo <erickzanardoo@gmail.com>
2019-07-06 17:52:20 -0300

    Adding a very simple title screen for now

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -5,24 +5,39 @@ import 'package:flutter/gestures.dart';
 import 'package:flame/flame.dart';
 
 import 'screens/game/game.dart';
+import 'screens/title_screen.dart';
 
 import 'game_data.dart';
+
+class Main {
+  static Game game;
+}
 
 void main() async {
   await Flame.init(fullScreen: true, orientation: DeviceOrientation.portraitUp);
 
-  final size = await Flame.util.initialDimensions();
-  final initialCoins = await GameData.getCoins();
+  runApp(new MaterialApp(
+    home: new Scaffold(body: TitleScreen()),
+    routes: {
+    },
+  )); 
 
-  final game = Game(size, initialCoins);
-  runApp(game.widget);
+  //final size = await Flame.util.initialDimensions();
+  //final initialCoins = await GameData.getCoins();
+
+  //final game = Game(size, initialCoins);
+  //runApp(game.widget);
 
   Flame.util.addGestureRecognizer(TapGestureRecognizer()
     ..onTapDown = (TapDownDetails evt) {
-      game.player.stop();
+      if (Main.game != null) {
+        Main.game.player.stop();
+      }
     }
     ..onTapUp = (TapUpDetails evt) {
-      game.player.resume();
+      if (Main.game != null) {
+        Main.game.player.resume();
+      }
     }
   );
 }
---
+++ game/lib/screens/title_screen.dart
@@ -0,0 +1,45 @@
+import "package:flutter/material.dart";
+import "package:flame/flame.dart";
+
+import "game/game.dart";
+import "../game_data.dart";
+import "../main.dart";
+
+class TitleScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TitleScreenState();
+}
+
+class _TitleScreenState extends State<TitleScreen> {
+  Widget build(BuildContext context) {
+    if (Main.game != null) {
+      return Main.game.widget;
+    }
+
+    return Center(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const SizedBox(height: 30),
+              RaisedButton(
+                  onPressed: () {
+                    startGame();
+                  },
+                  child: const Text(
+                      "Play",
+                      style: TextStyle(fontSize: 20)
+                  ),
+              ),
+            ],
+        ),
+    );
+  }
+
+  startGame() async {
+    final size = await Flame.util.initialDimensions();
+    final initialCoins = await GameData.getCoins();
+
+    Main.game = Game(size, initialCoins);
+    setState(() {});
+  }
+}
