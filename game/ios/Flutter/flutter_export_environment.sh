fb2aad6a2184c630777cefaaf8dc30f7f4d4593b
Erick Zanardo <erickzanardoo@gmail.com>
2019-10-03 22:03:37 -0300

    bumping version

--- game/ios/Flutter/flutter_export_environment.sh
+++ game/ios/Flutter/flutter_export_environment.sh
@@ -6,5 +6,5 @@ export "FLUTTER_TARGET=/Users/erick/projects/gamedev/bounce_box/game/lib/main.da
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/erick/flutter/bin/cache/artifacts/engine/ios-release"
-export "FLUTTER_BUILD_NAME=1.0.4"
-export "FLUTTER_BUILD_NUMBER=10"
+export "FLUTTER_BUILD_NAME=1.0.5"
+export "FLUTTER_BUILD_NUMBER=11"
